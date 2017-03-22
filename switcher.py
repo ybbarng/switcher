4a01b2578cc605e707b7d7553686836284dae193
ybbarng <ybbarng@gmail.com>
2017-03-22 15:22:33 +0900

    Match uuids and their descriptions

--- switcher.py
+++ switcher.py
@@ -5,6 +5,7 @@ class Switcher:
     switcher = None
     battery_handler = None
     switch_handler = None
+    uuids = None
 
     def __init__(self, address, address_type):
         retry = True
@@ -17,25 +18,44 @@ class Switcher:
                 print('Error on connecting')
                 print(e)
         try:
+            # self.retrieve_informations()
             self.run()
         finally:
             print('disconnect from the switcher')
             self.switcher.disconnect()
 
+    def retrieve_informations(self):
+        self.load_uuids()
+        self.get_services()
+        self.get_characteristics()
+
+    def load_uuids(self):
+        import json
+
+        with open('uuid.json') as f:
+            self.uuids = {v: k for k, v in json.load(f).items()}
+
+    def get_uuid_description(self, uuid):
+        try:
+            return self.uuids[uuid]
+        except:
+            return 'Unknown'
+
     def get_services(self):
         services = self.switcher.getServices()
         print('services')
         for service in services:
-            print(service.uuid)
+            uuid = str(service.uuid)
+            print('UUID: {} ({})'.format(uuid, self.get_uuid_description(uuid)))
 
     def get_characteristics(self):
         characteristics = self.switcher.getCharacteristics()
         print('characteristics')
         for ch in characteristics:
-            print(ch.uuid)
-            print(ch.properties)
-            print(ch.propertiesToString())
-            print(ch.getHandle())
+            uuid = str(ch.uuid)
+            print('UUID: {} ({})'.format(uuid, self.get_uuid_description(uuid)))
+            print('Properties: {} ({})'.format(ch.properties, ch.propertiesToString()))
+            print('Handler: 0x{:02x}'.format(ch.getHandle()))
             print('\n')
 
     def get_descriptors(self):
